5babdb21adc6b9f5a78241150d1b4be54938b77c
mariannapsyllou <117476668+mariannapsyllou@users.noreply.github.com>
2023-01-24 15:48:08 +0100

    Update Main.java

--- src/se/hkr/Main.java
+++ src/se/hkr/Main.java
@@ -13,21 +13,6 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         String regex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
-        String mail = "opåw+++@gmail.com";
-        //Pattern pattern = new Pattern.matches(regex);
-        Validation valid = Validation.isValid(mail);
-        System.out.println(Validation.isValid(mail));
-        System.out.println("DO you want to validate your email by input or by file");
-        /*File file = new File("/Users/mariannap/Desktop/HKR/Year 1/Methods of Sustainable Programming/ProjectEx1/Examination1/src/se/hkr/names.txt");
-        Scanner scanner = new Scanner(file);
-        List<String> namesEmails = new ArrayList<String>();
-        List<String> filteredNames = new ArrayList<>();
-        while(scanner.hasNextLine()){
-            namesEmails.add(scanner.nextLine());
-        }
-        Validation nameEmailValidation = Validation.valid.isValid(String mail);
-        filteredNames = namesEmails.stream().filter(nameEmailValidation(String mail))
-                .collect(Collectors.toList());*/
 
 
 
