8671347b838c15d5973c402ad2787d9a84ed26e8
mariannapsyllou <117476668+mariannapsyllou@users.noreply.github.com>
2023-01-25 13:58:39 +0100

    extra function added

--- src/se/hkr/Main.java
+++ src/se/hkr/Main.java
@@ -1,6 +1,6 @@
 package se.hkr;
 
-import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,6 +8,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -19,14 +20,24 @@ public class Main {
         Path path = Paths.get("/Users/mariannap/Desktop/HKR/Year 1/Methods of Sustainable Programming/ProjectEx1/Examination1/src/se/hkr/names.txt");
         List<String> validEmails = new ArrayList<>();
 
+
+        // If we want just to find out which emails are valid
         try(Stream<String> emails = Files.lines(path)){
              validEmails =emails.filter(emailValidation::isValid)
                     .collect(Collectors.toList());
              validEmails.forEach(System.out::println);
 
+        // Grouping emails depending on their domain
+            Map<String, List<String>> groupingEmails = validEmails.stream()
+                    .collect(Collectors.groupingBy(email->email.substring(email.indexOf("@") + 1)));
+            groupingEmails.entrySet().forEach(entry->{
+                        System.out.printf("Domain: " +"\n" + entry.getKey());
+                        System.out.println("\nEmails: ");
+                        entry.getValue().forEach(email-> System.out.println("" + email));
+            });
+
         }catch(IOException e){
             e.printStackTrace();
-
         }
     }
 }
